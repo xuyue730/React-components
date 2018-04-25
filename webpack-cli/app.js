60449cbb41d6e83b745decff67fed6f95a900881
xuyue <xuyue@lueen.io>
2018-04-25 09:15:03 +0800

    1

--- webpack-cli/app.js
+++ webpack-cli/app.js
@@ -4,7 +4,8 @@ import sum from './sum';
 
 var minus = require('./minus');
 
-
+// path.resolve(__dirname,'dist');
+// publishPath:'./dist/';
 
 console.log('sum(12,24) = ',sum(23,24));
 console.log('minus(24,17) = ',minus(24,17));
